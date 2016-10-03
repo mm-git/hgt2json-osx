2c029bf94d102775618716bc9364048784835315
Motoyasu Meguro <mmgithub@gmail.com>
2016-10-03 10:29:15 +0900

    Initial Commit

---
+++ hgt2json-osx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hgt2json-osx
+//
+//  Created by mm on 2016/10/03.
+//  Copyright © 2016年 code-house. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
